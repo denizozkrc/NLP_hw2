1585b52478bffbf85816bcda2c1a9349d50b5d48
denizozkiric <ozkiric.deniz@metu.edu.tr>
2024-12-30 21:17:23 +0300

    training and eval on bert complete

--- main.py
+++ main.py
@@ -7,94 +7,133 @@ from datasets import Dataset
 
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
 # model = AutoModel.from_pretrained("dbmdz/bert-base-turkish-cased")
-model = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)  # Adjust `num_labels`
+model1 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
+model2 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
 
 
-dataset1_raw = (open(file="./datasets/orientation-tr-train.tsv", mode="r")).read()
-# dataset1_inputs = []
-# dataset1_labels = []
-data1 = {"text": [], "label": []}
-for line in dataset1_raw.split("\n"):
-    if len(line) == 0:
-        continue
-    parts = line.split("\t")
-    if(parts[-1] == "label"):
-        continue
-    text_org = parts[-3]
-    text_eng = parts[-2]
-    # dataset1_inputs.append(text_org)
-    data1["text"].append(text_org)
-    data1["label"].append(int(parts[-1]))
-    # dataset1_labels.append(parts[-1])
-# del dataset1_inputs[0]
-# del dataset1_labels[0]
-del data1["text"][0]
-del data1["label"][0]
-dataset1 = Dataset.from_dict(data1)
-ds1 = dataset1
-
-dataset2_raw = (open(file="./datasets/power-tr-train.tsv", mode="r")).read()
-# dataset2_inputs = []
-# dataset2_labels = []
-data2 = {"text": [], "label": []}
-for line in dataset2_raw.split("\n"):
-    if len(line) == 0:
-        continue
-    parts = line.split("\t")
-    if(parts[-1] == "label"):
-        continue
-    text_org = parts[-3]
-    text_eng = parts[-2]
-    # dataset2_inputs.append(text_org)
-    # dataset2_labels.append(parts[-1])
-    data2["text"].append(text_org)
-    data2["label"].append(int(parts[-1]))
-# del dataset2_inputs[0]
-# del dataset2_labels[0]
-dataset2 = Dataset.from_dict(data2)
-
-# TODO: Stratified k-fold 1 to 9 for the shared task
-
-# inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-
-# Get model outputs
-# outputs = model(**inputs)
-
-training_args = TrainingArguments(
-    output_dir="./results",
-    eval_strategy="no",
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    num_train_epochs=3,
-    save_steps=10_000,
-    save_total_limit=2,
-)
-
-"""for i, text in enumerate(dataset1_inputs):
-    input = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-    input["labels"] = int(dataset1_labels[i])
-    dataset1.append(input)"""
-
-
-# Tokenize datasets
 def tokenize_function(example):
     return tokenizer(example["text"], padding="max_length", truncation=True)
+    # tokenizer(text, return_tensors="pt", padding=True, truncation=True)
 
 
-dataset1 = dataset1.map(tokenize_function, batched=True)
-dataset2 = dataset2.map(tokenize_function, batched=True)
+def execute_task1():
+    # on original text
+    dataset_raw = (open(file="./datasets/orientation-tr-train.tsv", mode="r")).read()
+    dataset_dict = {"text": [], "label": []}
+    for line in dataset_raw.split("\n"):
+        if len(line) == 0:
+            continue
+        parts = line.split("\t")
+        if (parts[-1] == "label"):
+            continue
+        text_org = parts[-3]
+        # text_eng = parts[-2]
+        dataset_dict["text"].append(text_org)
+        dataset_dict["label"].append(int(parts[-1]))
+    dataset = Dataset.from_dict(dataset_dict)
+    #ds1 = dataset1
+
+    # on original text
+    dataset_raw_test = (open(file="./datasets/orientation-tr-test.tsv", mode="r")).read()
+    dataset_dict_test = {"text": [], "label": []}
+    for line in dataset_raw_test.split("\n"):
+        if len(line) == 0:
+            continue
+        parts = line.split("\t")
+        if (parts[-1] == "label"):
+            continue
+        text_org = parts[-3]
+        # text_eng = parts[-2]
+        dataset_dict_test["text"].append(text_org)
+        dataset_dict_test["label"].append(int(parts[-1]))
+    dataset_test = Dataset.from_dict(dataset_dict_test)
+    #ds1 = dataset1
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        eval_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        num_train_epochs=3,
+        save_steps=10_000,
+        save_total_limit=2,
+    )
+
+    dataset = dataset.map(tokenize_function, batched=True)
+    dataset_test = dataset_test.map(tokenize_function, batched=True)
+
+    trainer = Trainer(
+        model=model1,
+        args=training_args,
+        train_dataset=dataset,
+        eval_dataset=dataset_test,
+    )
+
+    trainer.train()
+
+    evaluation_results = trainer.evaluate()
+    print("Evaluation Results task1:", evaluation_results)
+
+
+def execute_task2():
+    # on english translated text 
+    dataset_raw = (open(file="./datasets/power-tr-train.tsv", mode="r")).read()
+    dataset_dict = {"text": [], "label": []}
+    for line in dataset_raw.split("\n"):
+        if len(line) == 0:
+            continue
+        parts = line.split("\t")
+        if (parts[-1] == "label"):
+            continue
+        # text_org = parts[-3]
+        text_eng = parts[-2]
+        dataset_dict["text"].append(text_eng)
+        dataset_dict["label"].append(int(parts[-1]))
+    dataset = Dataset.from_dict(dataset_dict)
+
+    # on english translated text 
+    dataset_raw_test = (open(file="./datasets/power-tr-test.tsv", mode="r")).read()
+    dataset_dict_test = {"text": [], "label": []}
+    for line in dataset_raw_test.split("\n"):
+        if len(line) == 0:
+            continue
+        parts = line.split("\t")
+        if (parts[-1] == "label"):
+            continue
+        # text_org = parts[-3]
+        text_eng = parts[-2]
+        dataset_dict_test["text"].append(text_eng)
+        dataset_dict_test["label"].append(int(parts[-1]))
+    dataset_test = Dataset.from_dict(dataset_dict_test)
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        eval_strategy="no",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        num_train_epochs=3,
+        save_steps=10_000,
+        save_total_limit=2,
+    )
+
+    dataset = dataset.map(tokenize_function, batched=True)
+
+    trainer = Trainer(
+        model=model2,
+        args=training_args,
+        train_dataset=dataset,
+        eval_dataset=dataset_test, # use this later
+    )
+
+    trainer.train()
+
+    evaluation_results = trainer.evaluate()
+    print("Evaluation Results task2: ", evaluation_results)
+
+
+# TODO: Stratified k-fold 1 to 9 for the shared task
 
 # Tokenization check
 #for i in range(len(dataset1)):
 #    if(dataset1[i]["label"] != ds1[i]["label"]):
 #        print("Error in tokenization")
-
-
-"""trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=dataset1,
-    # eval_dataset=eval_dataset, # use this later
-)
-
-trainer.train()"""
