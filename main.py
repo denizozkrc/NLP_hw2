27afb7ef0cf4f23bd55b59c6b078017a449564ee
denizozkiric <ozkiric.deniz@metu.edu.tr>
2024-12-31 00:44:03 +0300

    llama partially implemented

--- main.py
+++ main.py
@@ -1,19 +1,74 @@
 # from sklearn.model_selection import StratifiedKFold
 import numpy as np
-from transformers import AutoTokenizer, AutoModel
-from transformers import Trainer, TrainingArguments
-from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer, Trainer, TrainingArguments, pipeline, AutoModelForSequenceClassification
 from datasets import Dataset
+import evaluate
 
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
+tokenizer_llama = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
+
 # model = AutoModel.from_pretrained("dbmdz/bert-base-turkish-cased")
 model1 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
 model2 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
+
+model_llama = AutoModelForSequenceClassification.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct", num_labels=2)
+
+metric = evaluate.load("accuracy")
+# Meta-Llama-3.1-8B-Instruct
+# TODO: max_length
+
+#def compute_metrics_llama(predictions, labels):
+#    preds = np.argmax(predictions, axis=-1)
+#    return metric.compute(predictions=preds, references=labels)
+
+
+def tokenize_function_llama(example):
+    return tokenizer_llama(example["text"], padding="max_length", truncation=True)
+
+
+def execute_llama_twice():
+    dataset_raw_test = (open(file="./datasets/orientation-tr-test.tsv", mode="r")).read()
+    dataset_dict_test = {"text": [], "label": []}
+    for line in dataset_raw_test.split("\n"):
+        if len(line) == 0:
+            continue
+        parts = line.split("\t")
+        if (parts[-1] == "label"):
+            continue
+        text_org = parts[-3]
+        # text_eng = parts[-2]
+        dataset_dict_test["text"].append(text_org)
+        dataset_dict_test["label"].append(int(parts[-1]))
+    dataset_test = Dataset.from_dict(dataset_dict_test)
+
+    dataset_test = dataset_test.map(tokenize_function_llama, batched=True)
+
+    classifier = pipeline("text-classification", model=model_llama, tokenizer=tokenizer_llama)
+    predictions = classifier(dataset_test["text"])
+
+    predicted_labels = []
+    for prediction in predictions:
+        predicted_labels.append(int(prediction["label"]))
+    
+    accuracy = 0
+    for true_l, predicted_l in zip(dataset_test["label"], predicted_labels):
+        if true_l == predicted_l:
+            accuracy += 1
+    accuracy = accuracy/len(predicted_labels)
+    print("Accuracy: ", accuracy)
+
+
 
 
 def tokenize_function(example):
     return tokenizer(example["text"], padding="max_length", truncation=True)
     # tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
 
 def execute_task1():
@@ -52,11 +107,6 @@ def execute_task1():
     training_args = TrainingArguments(
         output_dir="./results",
         eval_strategy="epoch",
-        learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        num_train_epochs=3,
-        save_steps=10_000,
-        save_total_limit=2,
     )
 
     dataset = dataset.map(tokenize_function, batched=True)
@@ -67,6 +117,7 @@ def execute_task1():
         args=training_args,
         train_dataset=dataset,
         eval_dataset=dataset_test,
+        compute_metrics=compute_metrics,
     )
 
     trainer.train()
@@ -109,11 +160,6 @@ def execute_task2():
     training_args = TrainingArguments(
         output_dir="./results",
         eval_strategy="no",
-        learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        num_train_epochs=3,
-        save_steps=10_000,
-        save_total_limit=2,
     )
 
     dataset = dataset.map(tokenize_function, batched=True)
@@ -122,7 +168,8 @@ def execute_task2():
         model=model2,
         args=training_args,
         train_dataset=dataset,
-        eval_dataset=dataset_test, # use this later
+        eval_dataset=dataset_test,
+        compute_metrics=compute_metrics,
     )
 
     trainer.train()
