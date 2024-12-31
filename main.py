9422f1bf0bcc0692f608d1525ee40f840938c0e5
denizozkiric <ozkiric.deniz@metu.edu.tr>
2024-12-31 13:24:17 +0300

    save before major change

--- main.py
+++ main.py
@@ -1,60 +1,61 @@
-# from sklearn.model_selection import StratifiedKFold
 import numpy as np
-from transformers import AutoTokenizer, Trainer, TrainingArguments, pipeline, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, Trainer, TrainingArguments, pipeline, AutoModelForSequenceClassification, GPT2Tokenizer, GPT2ForSequenceClassification
 from datasets import Dataset
 import evaluate
+from sklearn.model_selection import train_test_split
+
 
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
-tokenizer_llama = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
+tokenizer_gpt2 = GPT2Tokenizer.from_pretrained("gpt2")
 
 # model = AutoModel.from_pretrained("dbmdz/bert-base-turkish-cased")
 model1 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
 model2 = AutoModelForSequenceClassification.from_pretrained("dbmdz/bert-base-turkish-cased", num_labels=2)
 
-model_llama = AutoModelForSequenceClassification.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct", num_labels=2)
+model_gpt2 = GPT2ForSequenceClassification.from_pretrained("gpt2", num_labels=2)
 
 metric = evaluate.load("accuracy")
-# Meta-Llama-3.1-8B-Instruct
 # TODO: max_length
 
-#def compute_metrics_llama(predictions, labels):
-#    preds = np.argmax(predictions, axis=-1)
-#    return metric.compute(predictions=preds, references=labels)
+
+def tokenize_function_gpt2(example):
+    return tokenizer_gpt2(example["text"], padding="max_length", truncation=True)
 
 
-def tokenize_function_llama(example):
-    return tokenizer_llama(example["text"], padding="max_length", truncation=True)
-
-
-def execute_llama(org_lang: bool, file_name: str):
-    dataset_raw_test = (open(file=file_name, mode="r")).read()
+def execute_gpt2(org_lang: bool, file_name: str):
+    i=0
+    # Load the raw test dataset
+    dataset_raw_test = open(file=file_name, mode="r").read()
     dataset_dict_test = {"text": [], "label": []}
+
     for line in dataset_raw_test.split("\n"):
+        i += 1
+        if i == 100:
+            break
         if len(line) == 0:
             continue
         parts = line.split("\t")
-        if (parts[-1] == "label"):
+        if parts[-1] == "label":
             continue
         text_org = parts[-3]
         text_eng = parts[-2]
         dataset_dict_test["text"].append(text_org) if org_lang else dataset_dict_test["text"].append(text_eng)
         dataset_dict_test["label"].append(int(parts[-1]))
     dataset_test = Dataset.from_dict(dataset_dict_test)
+    dataset_test = dataset_test.map(tokenize_function_gpt2, batched=True)
 
-    dataset_test = dataset_test.map(tokenize_function_llama, batched=True)
-
-    classifier = pipeline("text-classification", model=model_llama, tokenizer=tokenizer_llama)
+    classifier = pipeline("text-classification", model=model_gpt2, tokenizer=tokenizer_gpt2)
     predictions = classifier(dataset_test["text"])
 
     predicted_labels = []
     for prediction in predictions:
         predicted_labels.append(int(prediction["label"]))
-    
+
     accuracy = 0
     for true_l, predicted_l in zip(dataset_test["label"], predicted_labels):
         if true_l == predicted_l:
             accuracy += 1
-    accuracy = accuracy/len(predicted_labels)
+    accuracy = accuracy / len(predicted_labels)
     print("Accuracy: ", accuracy)
 
 
@@ -71,9 +72,13 @@ def compute_metrics(eval_pred):
 
 def execute_task1():
     # on original text
+    i=0
     dataset_raw = (open(file="./datasets/orientation-tr-train.tsv", mode="r")).read()
     dataset_dict = {"text": [], "label": []}
     for line in dataset_raw.split("\n"):
+        i += 1
+        if i == 100:
+            break
         if len(line) == 0:
             continue
         parts = line.split("\t")
@@ -84,17 +89,21 @@ def execute_task1():
         dataset_dict["text"].append(text_org)
         dataset_dict["label"].append(int(parts[-1]))
     dataset = Dataset.from_dict(dataset_dict)
-    #ds1 = dataset1
 
     # on original text
+    i=0
     dataset_raw_test = (open(file="./datasets/orientation-tr-test.tsv", mode="r")).read()
     dataset_dict_test = {"text": [], "label": []}
     for line in dataset_raw_test.split("\n"):
+        i += 1
+        if i ==100:
+            break;
         if len(line) == 0:
             continue
         parts = line.split("\t")
         if (parts[-1] == "label"):
             continue
+        print(parts)
         text_org = parts[-3]
         # text_eng = parts[-2]
         dataset_dict_test["text"].append(text_org)
@@ -123,15 +132,19 @@ def execute_task1():
     evaluation_results = trainer.evaluate()
     print("Evaluation Results task1:", evaluation_results)
 
-    execute_llama(True, "./datasets/orientation-tr-test.tsv")  # on original lang (tr)
-    execute_llama(False, "./datasets/orientation-tr-test.tsv")  # on english
+    execute_gpt2(True, "./datasets/orientation-tr-test.tsv")  # on original lang (tr)
+    execute_gpt2(False, "./datasets/orientation-tr-test.tsv")  # on english
 
 
 def execute_task2():
     # on english translated text 
+    i=0
     dataset_raw = (open(file="./datasets/power-tr-train.tsv", mode="r")).read()
     dataset_dict = {"text": [], "label": []}
     for line in dataset_raw.split("\n"):
+        i += 1
+        if i ==100:
+            break;
         if len(line) == 0:
             continue
         parts = line.split("\t")
@@ -144,9 +157,13 @@ def execute_task2():
     dataset = Dataset.from_dict(dataset_dict)
 
     # on english translated text 
+    i=0
     dataset_raw_test = (open(file="./datasets/power-tr-test.tsv", mode="r")).read()
     dataset_dict_test = {"text": [], "label": []}
     for line in dataset_raw_test.split("\n"):
+        i += 1
+        if i ==100:
+            break;
         if len(line) == 0:
             continue
         parts = line.split("\t")
@@ -178,8 +195,8 @@ def execute_task2():
     evaluation_results = trainer.evaluate()
     print("Evaluation Results task2: ", evaluation_results)
 
-    execute_llama(True, "./datasets/power-tr-test.tsv")  # on original lang (tr)
-    execute_llama(False, "./datasets/power-tr-test.tsv")  # on english
+    execute_gpt2(True, "./datasets/power-tr-test.tsv")  # on original lang (tr)
+    execute_gpt2(False, "./datasets/power-tr-test.tsv")  # on english
 
 
 # TODO: Stratified k-fold 1 to 9 for the shared task
@@ -188,3 +205,6 @@ def execute_task2():
 #for i in range(len(dataset1)):
 #    if(dataset1[i]["label"] != ds1[i]["label"]):
 #        print("Error in tokenization")
+
+execute_task1()
+# execute_task2()
